ef9ac2565d6b898d60a70e21b9c7116f52612ddf
George Vorobyov <georgevorobyov37@gmail.com>
2020-02-12 15:04:41 +0100

    change Header component to Hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,56 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { List, ListItem, BurgerButton, HeaderPage } from "./Header.styles";
 
-class Header extends React.Component {
-  state = {
-    mobileMenuOpened: false
+function Header() {
+  const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
+
+  const toggleMenu = () => {
+    // this.setState(prevState => ({
+    //   mobileMenuOpened: !prevState.mobileMenuOpened
+    // }));
+    setMobileMenuOpened(!mobileMenuOpened);
   };
 
-  toggleMenu = () => {
-    this.setState(prevState => ({
-      mobileMenuOpened: !prevState.mobileMenuOpened
-    }));
-  };
+  return (
+    <HeaderPage>
+      <List className={mobileMenuOpened ? "opened" : ""}>
+        <ListItem>
+          <NavLink exact to="/" activeClassName="is-active">
+            HOME
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink to="/discography" activeClassName="is-active">
+            DISCOGRAPHY
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink to="/concert_tours" activeClassName="is-active">
+            Concert tours
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink to="/compositions_list" activeClassName="is-active">
+            latter compositions
+          </NavLink>
+        </ListItem>
+        <ListItem>
+          <NavLink to="/slider_comments" activeClassName="is-active">
+            Upcoming Events
+          </NavLink>
+        </ListItem>
+        <ListItem>History </ListItem>
+        <ListItem>contact</ListItem>
+      </List>
 
-  render() {
-    return (
-      <HeaderPage>
-        <List className={this.state.mobileMenuOpened ? "opened" : ""}>
-          <ListItem>
-            <NavLink exact to="/" activeClassName="is-active">
-              HOME
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/discography" activeClassName="is-active">
-              DISCOGRAPHY
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/concert_tours" activeClassName="is-active">
-              Concert tours
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/compositions_list" activeClassName="is-active">
-              latter compositions
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/slider_comments" activeClassName="is-active">
-              Upcoming Events
-            </NavLink>
-          </ListItem>
-          <ListItem>History </ListItem>
-          <ListItem>contact</ListItem>
-        </List>
-
-        <BurgerButton onClick={this.toggleMenu}>
-          <i className="fa fa-bars"></i>
-        </BurgerButton>
-      </HeaderPage>
-    );
-  }
+      <BurgerButton onClick={toggleMenu}>
+        <i className="fa fa-bars"></i>
+      </BurgerButton>
+    </HeaderPage>
+  );
 }
 export default Header;
