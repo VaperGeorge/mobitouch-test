98fea28cb840a60b2d4bd97449a0576184446d53
George Vorobyov <georgevorobyov37@gmail.com>
2020-02-13 09:51:35 +0100

    change Compositions list to Hooks

--- src/components/Compositions/CompositionsList.js
+++ src/components/Compositions/CompositionsList.js
@@ -1,117 +1,113 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SectionHeader, AddNewComposition } from "./../index";
-import { Section, AddNewButtonWrapper, CompositionList } from "./Compositions.styles";
-import { connect } from 'react-redux';
-import { compositionsFetchData, removeComposition, getCompositionById } from '../../actions/compositions';
+import {
+  Section,
+  AddNewButtonWrapper,
+  CompositionList
+} from "./Compositions.styles";
+import { shallowEqual, useSelector, useDispatch } from "react-redux";
+import {
+  compositionsFetchData,
+  removeComposition,
+  getCompositionById
+} from "../../actions/compositions";
 import CompositionItem from "./CompositionItem";
 
 let title = " latter compositions";
-let desc = "<It's Time> was released as the lead single from Continued Silence and It's Time, both extended plays preceding Night Visions' release.";
+let desc =
+  "<It's Time> was released as the lead single from Continued Silence and It's Time, both extended plays preceding Night Visions' release.";
 
-class CompositionsList extends React.Component {
+const CompositionsList = props => {
+  const [showModal, setshowModal] = useState(false);
+  const [isItemEdit, setisItemEdit] = useState(false);
 
-  state = {
-    showModal: false,
-    isItemEdit: false
-  }
+  const { items, hasErrored, isLoading } = useSelector(
+    state => ({
+      items: state.compositions.compositions,
+      hasErrored: state.compositions.itemsHasErrored,
+      isLoading: state.compositions.itemsIsLoading
+    }),
+    shallowEqual
+  );
 
-  componentDidMount() {
-    this.props.fetchCompositions()
-  }
+  const dispatch = useDispatch();
 
-  addNewPost = () => {
-    this.setState(() => ({
-      isItemEdit: false
-    }))
-    this.toggleModalShow();
-  }
+  useEffect(() => {
+    dispatch(compositionsFetchData());
+  }, [props, dispatch]);
 
-  toggleModalShow() {
-    this.setState(prevState => ({
-      showModal: !prevState.showModal
-    }))
-  }
-
-  deletePost = (id) => {
-    this.props.deleteComposition(id).then(() => {
-      this.props.fetchCompositions()
-    })
-  }
-
-  editPost = (id) => {
-    this.toggleModalShow();
-    this.props.getCompositionById(id).then((item) => {
-      this.setState(() => ({
-        isItemEdit: true
-      }))
-    })
-  }
-
-  render() {
-    if (this.props.hasErrored) {
-      return (
-        <Section>
-          <div className="container">
-
-            <SectionHeader title={title} desc={desc} />
-
-            <p>Sorry! There was an error loading the items</p>
-          </div>
-        </Section>
-      )
-    }
-    if (this.props.isLoading) {
-      return (
-        <Section>
-          <div className="container">
-
-            <SectionHeader title={title} desc={desc} />
-
-            <p>Loading compositions...</p>
-          </div>
-        </Section>
-      )
-    }
-    if (this.props.items) {
-      return (
-        <Section>
-          <div className="container">
-
-            <SectionHeader title={title} desc={desc} />
-
-            <CompositionList>
-              {this.props.items.map((item, idx) => (
-                <CompositionItem item={item} key={idx} deletePost={this.deletePost} editPost={this.editPost} />
-              ))}
-            </CompositionList>
-            <AddNewButtonWrapper>
-              <button onClick={() => this.addNewPost()}>Add new Composition</button>
-            </AddNewButtonWrapper>
-          </div>
-          <AddNewComposition
-            isItemEdit={this.state.isItemEdit}
-            show={this.state.showModal}
-            onHide={() => this.toggleModalShow()} />
-        </Section>
-      );
-    }
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    items: state.compositions.compositions,
-    hasErrored: state.compositions.itemsHasErrored,
-    isLoading: state.compositions.itemsIsLoading
+  const addNewPost = () => {
+    setisItemEdit(false);
+    toggleModalShow();
   };
+
+  const toggleModalShow = () => {
+    setshowModal(!showModal);
+  };
+
+  const deletePost = id => {
+    dispatch(removeComposition(id)).then(() => {
+      dispatch(compositionsFetchData());
+    });
+  };
+
+  const editPost = id => {
+    toggleModalShow();
+    dispatch(getCompositionById(id)).then(item => {
+      setisItemEdit(true);
+    });
+  };
+
+  if (hasErrored) {
+    return (
+      <Section>
+        <div className="container">
+          <SectionHeader title={title} desc={desc} />
+
+          <p>Sorry! There was an error loading the items</p>
+        </div>
+      </Section>
+    );
+  }
+  if (isLoading) {
+    return (
+      <Section>
+        <div className="container">
+          <SectionHeader title={title} desc={desc} />
+
+          <p>Loading compositions...</p>
+        </div>
+      </Section>
+    );
+  }
+  if (items) {
+    return (
+      <Section>
+        <div className="container">
+          <SectionHeader title={title} desc={desc} />
+
+          <CompositionList>
+            {items.map((item, idx) => (
+              <CompositionItem
+                item={item}
+                key={idx}
+                deletePost={deletePost}
+                editPost={editPost}
+              />
+            ))}
+          </CompositionList>
+          <AddNewButtonWrapper>
+            <button onClick={() => addNewPost()}>Add new Composition</button>
+          </AddNewButtonWrapper>
+        </div>
+        <AddNewComposition
+          isItemEdit={isItemEdit}
+          show={showModal}
+          onHide={() => toggleModalShow()}
+        />
+      </Section>
+    );
+  }
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    deleteComposition: id => dispatch(removeComposition(id)),
-    fetchCompositions: () => dispatch(compositionsFetchData()),
-    getCompositionById: id => dispatch(getCompositionById(id)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CompositionsList);
+export default CompositionsList;
